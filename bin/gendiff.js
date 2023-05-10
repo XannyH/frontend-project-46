8492e63695303525aa6f7a5562423c0691d6773d
Daniil <robertplaudis674@gmail.com>
2023-05-10 15:38:25 +0300

    update gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,8 @@
+const { Command } = require('commander');
+const program = new Command();
+
+program.version('0.0.1');
+
+program.description('Compares two configuration files and shows a difference.');
+
+program.parse();
